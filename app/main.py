44c5db20a6588d47fd6ef02568d85a2e1e09d931
MurderPumpkin <fer.brirent3@gmail.com>
2024-11-21 00:33:22 -0800

    Archivos donde estoy atorado charts country/Territory

--- app/main.py
+++ app/main.py
@@ -3,16 +3,24 @@ import read_csv
 import charts
 
 def run():
-    data = read_csv.read_csv('data.csv')
-    country = input('Type Country => ')
+  data = read_csv.read_csv('data.csv')
+  data = list(filter(lambda item : item['Continent'] == 'South America',data))
+  data_dict = data[0]
 
-    result = utils.population_by_country(data, country)
+  countries = list(map(lambda x: x['Country'], data))
+  percentages = list(map(lambda x: x['World Population Percentage'], data))
+  charts.generate_pie_chart(data_dict['Continent'], countries, percentages)
 
-    if len(result) > 0:
-        country = result[0]
-        labels, values = utils.get_population(country)
-        charts.generate_bar_chart(labels, values)
+  country = input('Type Country => ')
+  print(country)
 
-    
+  result = utils.population_by_country(data, country)
+
+  if len(result) > 0:
+    country = result[0]
+    print(country)
+    labels, values = utils.get_population(country)
+    charts.generate_bar_chart(country['Country'], labels, values)
+
 if __name__ == '__main__':
-    run()
+  run()
